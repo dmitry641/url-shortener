0ba7f54811615a7ee43ebbe34fad11eb0f5e4568
Dmitry <dmitry6416@gmail.com>
2020-08-24 00:24:57 +0300

    auth, login, register, jwt

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  links: [{ type: mongoose.Types.ObjectId, ref: "Link" }],
+  date: { type: Date, default: Date.now },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
