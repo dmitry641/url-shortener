60fb90bf6d5ac17107c16e5897847a42053927ec
Dmitry <dmitry6416@gmail.com>
2020-08-24 18:53:35 +0300

    update auth

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { useAuth } from "./hooks/auth.hook";
 import { AuthContext } from "./context/AuthContext";
 import "materialize-css";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   const { token, login, logout, userId } = useAuth();
@@ -14,6 +15,7 @@ function App() {
       value={{ token, login, logout, userId, isAuthenticated }}
     >
       <Router>
+        {isAuthenticated && <Navbar></Navbar>}
         <div className="container">{routes}</div>
       </Router>
     </AuthContext.Provider>
