5069963aac953f21263a3faab16fb80a4fa8d222
Dmitry <dmitry6416@gmail.com>
2020-08-24 18:30:05 +0300

    Authenticate, hooks, context

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 require("dotenv").config({ path: "./config/config.env" });
 require("./config/connectDB")();
 
+app.use(express.json({ extended: true }));
+
 app.use("/api/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 5000;
