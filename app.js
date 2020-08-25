cc121496b02b3455049ff56cfda2afdc7ed0366a
Dmitry <dmitry6416@gmail.com>
2020-08-25 10:43:25 +0300

    links backend

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ require("./config/connectDB")();
 app.use(express.json({ extended: true }));
 
 app.use("/api/auth", require("./routes/auth"));
+app.use("/api/link", require("./routes/link"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("App has been started..."));
